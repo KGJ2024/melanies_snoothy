502e57f928b77859bfb4baf1ce08dec597347ffe
KGJ2024 <govindkavi09@gmail.com>
2025-09-10 16:08:44 +0200

    Update streamlit_app.py
    json

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ st.write("The name will be", name_on_order)
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
 
